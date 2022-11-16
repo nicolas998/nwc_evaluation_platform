a69727546aa1778c83e9ed16eb1e2b099ec90515
nicolas998 <nicolas@uiowa>
2022-11-16 11:07:01 -0600

    updates readme.md to present project progress

--- pre_process/hrrr_conditional_mean_rel_error.py
+++ pre_process/hrrr_conditional_mean_rel_error.py
@@ -4,23 +4,6 @@ import netCDF4
 import numpy.ma as ma
 import numpy as np 
 
-year = 2019
-rmin = 4
-prod = 'nwcup'
-
-#re = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/%d_nwc_error.nc' % year)
-usgs = pd.read_csv('../data/usgs_joined_rlf.csv', index_col=0)
-if prod == 'nwc' or prod == 'per':
-    nwc_up = netCDF4.Dataset('../data/%d_nwc_error.nc' % year)
-else:
-    nwc_up = netCDF4.Dataset('../data/%d_nwc_error_upsegment.nc' % year)
-data = np.load('../data/hrrr_basin_'+str(year)+'.npz', allow_pickle = True)
-hrrr_totals = data['data']
-hrrr_usgs = data['usgs_id']
-hrrr_areas = pd.read_csv('../data/hrrr_usgs_areas.csv', index_col = 0)
-hrrr_rain = np.divide(hrrr_totals, hrrr_areas.values)
-shared = np.isin(hrrr_usgs, nwc_up['usgs_id']).nonzero()[0]
-
 def compute_mean_rel_error(lead, nwc_data,prod,rcond = 0):
     me = []    
     _nwc = nwc_data[prod][:,lead,:]
@@ -34,18 +17,40 @@ def compute_mean_rel_error(lead, nwc_data,prod,rcond = 0):
     idx = usgs.index.intersection(me.index)
     return me 
 
-if len(prod) == 3:
-    prod2eval = 'er_%s' % prod
-else:
-    prod2eval = 'er_%s' % prod[:3]
-for rmin in [3,]:
-    me = []
-    for lead in range(18):    
-        me.append(compute_mean_rel_error(lead,nwc_up,prod2eval, rmin))
-        lname = lead + 1
-        #me.to_pickle('../data/%d_lead_%d_nwcup_mean_rel_error_uncond.gz' % (year, lname))
-        print(lead)
-    me = pd.concat(me, axis = 1)
-    me.to_pickle('../data/%d_%s_mean_rel_error_hrrr_%d.gz' % (year,prod, rmin))
-    
-nwc_up.close()
+#re = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/%d_nwc_error.nc' % year)
+usgs = pd.read_csv('../data/usgs_joined_rlf.csv', index_col=0)
+
+for year in [2019, 2020, 2021, 2022]:
+    print(year)
+    for prod in ['nwc','per', 'nwcup']:
+
+        if prod == 'nwc' or prod == 'per':
+            nwc_up = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/%d_nwc_error.nc' % year)
+        else:
+            nwc_up = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/data/%d_nwc_error_upsegment.nc' % year)
+        data = np.load('../data/hrrr_basin_'+str(year)+'.npz', allow_pickle = True)
+        hrrr_totals = data['data']
+        hrrr_usgs = data['usgs_id']
+        hrrr_areas = pd.read_csv('../data/hrrr_usgs_areas.csv', index_col = 0)
+        hrrr_rain = np.divide(hrrr_totals, hrrr_areas.values)
+        shared = np.isin(hrrr_usgs, nwc_up['usgs_id']).nonzero()[0]
+
+
+
+        if len(prod) == 3:
+            prod2eval = 'er_%s' % prod
+        else:
+            prod2eval = 'er_%s' % prod[:3]
+        for rmin in [1,2,3,4]:
+            me = []
+            for lead in range(18):    
+                me.append(compute_mean_rel_error(lead,nwc_up,prod2eval, rmin))
+                lname = lead + 1
+                #me.to_pickle('../data/%d_lead_%d_nwcup_mean_rel_error_uncond.gz' % (year, lname))
+                #print(lead)
+            me = pd.concat(me, axis = 1)
+            me.to_pickle('../data/%d_%s_mean_rel_error_hrrr_%d.gz' % (year,prod, rmin))
+            print(rmin)
+        nwc_up.close()
+        print(prod)
+    print(year)
--- pre_process/independent_mean_rel_error.py
+++ pre_process/independent_mean_rel_error.py
@@ -4,12 +4,6 @@ import netCDF4
 import numpy.ma as ma
 import numpy as np 
 
-year = 2019
-
-#re = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/%d_nwc_error.nc' % year)
-usgs = pd.read_csv('../data/usgs_joined_rlf.csv', index_col=0)
-nwc_up = netCDF4.Dataset('../data/%d_nwc_error.nc' % year)
-
 def compute_mean_rel_error(lead, nwc_data,prod,qcond = 0):
     me = []    
     _nwc = nwc_data[prod][:,lead,:]
@@ -21,13 +15,33 @@ def compute_mean_rel_error(lead, nwc_data,prod,qcond = 0):
     idx = usgs.index.intersection(me.index)
     return me 
 
-me = []
-for lead in range(18):
-    me.append(compute_mean_rel_error(lead,nwc_up,'er_per'))
-    lname = lead + 1
-    #me.to_pickle('../data/%d_lead_%d_nwcup_mean_rel_error_uncond.gz' % (year, lname))
-    print(lead)
-me = pd.concat(me, axis = 1)
-me.to_pickle('../data/%d_per_mean_rel_error_uncond.gz' % year)
+for year in [2019,2020,2021]:
+    print(year)
+    for prod in ['nwc','per']:
+
+    #re = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/%d_nwc_error.nc' % year)
+        usgs = pd.read_csv('../data/usgs_joined_rlf.csv', index_col=0)
+        if prod == 'nwc' or prod == 'per':
+            nwc_up = netCDF4.Dataset('../source/%d_nwc_error.nc' % year)
+            #nwc_up = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/%d_nwc_error.nc' % year)
+        else:
+            nwc_up = netCDF4.Dataset('../data/%d_nwc_error_upsegment.nc' % year)
+            #nwc_up = netCDF4.Dataset('/mnt/y/flow/NWC_persistence_rel_error/%d_nwc_error_upsegment.nc' % year)
+
+        if len(prod) == 3:
+            prod2eval = 'er_%s' % prod
+        else:
+            prod2eval = 'er_%s' % prod[:3]
+
+        me = []
+        for lead in range(18):
+            me.append(compute_mean_rel_error(lead,nwc_up,prod2eval))
+            lname = lead + 1
+            #me.to_pickle('../data/%d_lead_%d_nwcup_mean_rel_error_uncond.gz' % (year, lname))
+            print(lead)
+        me = pd.concat(me, axis = 1)
+        me.to_pickle('../data/%d_%s_mean_rel_error_uncond.gz' % (year, prod))
+        nwc_up.close()
+        
+        print(prod)
     
-nwc_up.close()
